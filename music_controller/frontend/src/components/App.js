e30b66617af2f6cfca5a5e7274395e017a0cb36d
vaisrav <vaisrav@gmail.com>
2024-08-02 12:01:56 -0400

    react routes updated

--- music_controller/frontend/src/components/App.js
+++ music_controller/frontend/src/components/App.js
@@ -1,8 +1,7 @@
 import React, {Component} from "react";
 import { render } from "react-dom";
 import HomePage from "./HomePage";
-import RoomJoinPage from "./RoomJoinPage";
-import CreateJoinPage from "./CreateRoomPage";
+
 
 export default class App extends Component {
     constructor(props) {
@@ -12,8 +11,6 @@ export default class App extends Component {
     render() {
         return ( <div>
             <HomePage />
-            <RoomJoinPage />
-            <CreateJoinPage />
             </div> )
     }
 }
--- music_controller/frontend/src/components/HomePage.js
+++ music_controller/frontend/src/components/HomePage.js
@@ -1,12 +1,23 @@
 import { Component } from "react";
 import React from "react";
+import RoomJoinPage from "./RoomJoinPage";
+import CreateRoomPage from "./CreateRoomPage";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 export default class HomePage extends Component {
     constructor(props) {
-        super(props);
+      super(props);
     }
-
-    render(){
-        return <p>this is home page</p>
+  
+    render() {
+      return (
+        <Router>
+          <Routes>
+            <Route path="/" element={<p>This is the home page</p>} />
+            <Route path="/join" element={<RoomJoinPage />} />
+            <Route path="/create" element={<CreateRoomPage />} />
+          </Routes>
+        </Router>
+      );
     }
-}
+  }
